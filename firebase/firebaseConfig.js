9696be46a41349e5a25eb24152e5586fdd182317
Picknaut <admin@picknaut.com>
2025-06-18 22:30:02 +0100

    Add files via upload

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -9,7 +9,7 @@ if (!admin.apps.length) {
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
   });
 }
 
